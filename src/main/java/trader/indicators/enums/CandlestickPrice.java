1108af21c0d3b71e2d740ef33a72ea20d3e9620d
Stanislav Minchev <forexftg@yahoo.com>
2019-03-13 00:34:58 +0200

    refactor appliedPrice, change name to candlestickPrice, change associated interface to candlestickPriceOperations

---
+++ src/main/java/trader/indicators/enums/CandlestickPrice.java
@@ -0,0 +1,50 @@
+package trader.indicators.enums;
+
+
+import com.oanda.v20.instrument.CandlestickData;
+import trader.indicators.CandlestickPriceOperations;
+
+import java.math.BigDecimal;
+
+
+public enum CandlestickPrice implements CandlestickPriceOperations {
+
+    OPEN {
+        @Override
+        public BigDecimal extractPrice(CandlestickData candle) {
+            return candle.getO().bigDecimalValue();
+        }
+    },
+
+    CLOSE {
+        @Override
+        public BigDecimal extractPrice(CandlestickData candle){
+            return candle.getC().bigDecimalValue();
+        }
+    },
+
+    HIGH {
+        @Override
+        public BigDecimal extractPrice(CandlestickData candle){
+            return candle.getH().bigDecimalValue();
+        }
+    },
+
+    LOW {
+        @Override
+        public BigDecimal extractPrice(CandlestickData candle) {
+            return candle.getL().bigDecimalValue();
+        }
+    },
+
+    MEDIAN {
+        @Override
+        public BigDecimal extractPrice(CandlestickData candle) {
+            return medianPrice(candle.getH().bigDecimalValue(), candle.getL().bigDecimalValue());
+        }
+        
+        private BigDecimal medianPrice(BigDecimal high, BigDecimal low) {
+            return high.add(low).divide(BigDecimal.valueOf(2), BigDecimal.ROUND_HALF_UP);
+        }
+    }
+}
