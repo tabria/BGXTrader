fc43a7c1e78e5ab008c62feb2b5d92629576ade5
Stanislav Minchev <forexftg@yahoo.com>
2019-04-02 15:33:07 +0300

    add Configuration class for bgx strategy

--- src/test/java/trader/strategy/bgxstrategy/configuration/ConfigurationTest.java
+++ src/test/java/trader/strategy/bgxstrategy/configuration/ConfigurationTest.java
@@ -3,14 +3,21 @@ package trader.strategy.bgxstrategy.configuration;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.matchers.Null;
 import trader.exception.NullArgumentException;
+import trader.exception.OverflowException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ConfigurationTest {
 
+    private static final String DEFAULT_INSTRUMENT = "EUR_USD";
+    private static final Long DEFAULT_INITIAL_CANDLES_QUANTITY = 4999L;
+    private static final Long DEFAULT_UPDATE_CANDLES_QUANTITY = 2L;
 
     private Configuration config;
 
@@ -38,18 +45,49 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void WhenCallGetGranularity_ReturnCorrectType(){
-//        config.getGranularity()
+    public void WhenCallGetInstrument_ReturnCorrectType(){
+        assertEquals(String.class, config.getInstrument().getClass());
     }
 
+    @Test
+    public void WhenCreateThenDefaultInstrument(){
+        assertEquals(DEFAULT_INSTRUMENT, config.getInstrument());
+    }
 
+    @Test
+    public void WhenCallGetCandlesQuantity_ReturnCorrectType(){
+        assertEquals(List.class, config.getCandlesQuantity().getClass());
+    }
 
-//    systemGranularity:
-//    granularity: "M30"
-//    instrument:
-//    instrumentName: "EUR_USD"
-//    candlesQuantity:
-//    initial: "4999"
-//    update: "2"
+    @Test
+    public void WhenCreateThenDefaultQuantities(){
+        assertDefaultValues();
+    }
 
+    private void assertDefaultValues() {
+        List<Long> candlesQuantity = config.getCandlesQuantity();
+        assertEquals(DEFAULT_INITIAL_CANDLES_QUANTITY, candlesQuantity.get(0));
+        assertEquals(DEFAULT_UPDATE_CANDLES_QUANTITY, candlesQuantity.get(1));
+    }
+
+    @Test(expected = NullArgumentException.class)
+    public void WhenCallSetCandlesQuantityWithNull_Exception(){
+        config.setCandlesQuantity(null);
+    }
+
+    @Test
+    public void WhenCallSetCandlesWithEmptyQuantities_Defaults(){
+        config.setCandlesQuantity(new ArrayList<>());
+
+        assertDefaultValues();
+    }
+
+    @Test(expected = OverflowException.class)
+    public void WhenCallSetCandlesQuantityWithListWithMoreThanMaxElement_Exception(){
+        ArrayList<Long> quantities = new ArrayList<>();
+        quantities.add(1L);
+        quantities.add(2L);
+        quantities.add(3L);
+        config .setCandlesQuantity(quantities);
+    }
 }
